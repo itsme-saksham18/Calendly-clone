28300f522cfcc82591e86ffc2d48d6d59cb80b89
Saksham Maheshwari <sakshammaheshwari18@gmail.com>
2026-03-28 15:35:12 +0530

    Changed index.js cors url from local to client

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
 }));
 
